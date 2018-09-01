a610600b62a00c6d6d59c0ba0fd90a50e1e13fc4
icl <you@example.com>
2018-09-01 08:00:04 +0800

    insert time

--- docker/docker_jenkins/backupJenkins.sh
+++ docker/docker_jenkins/backupJenkins.sh
@@ -3,3 +3,10 @@
 path=/home/icl/docker-storage/jenkins
 tar cvf $path.tar $path
 
+timestamp="$(date +'%Y%m%d%H%M')"
+if [ -d Log ]; then
+  mkdir Log/$timestamp
+else
+  mkdir -p Log/$timestamp
+fi
+
